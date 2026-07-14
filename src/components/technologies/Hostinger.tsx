418fd0bcbb98e23e3c0cc6fe895eec7c36bc231e
aayush <ar0671362@gmail.com>
2026-07-14 07:17:40 +0530

    added hostinger

---
+++ src/components/technologies/Hostinger.tsx
@@ -0,0 +1,10 @@
+export default function Hostinger() {
+  return (
+    <svg viewBox="0 0 24 24" fill="none">
+      <path
+        d="M4 3H8V10H16V3H20V21H16V14H8V21H4V3Z"
+        fill="#673DE6"
+      />
+    </svg>
+  );
+}
