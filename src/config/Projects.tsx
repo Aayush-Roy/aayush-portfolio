23e7000862834a63c8ac04feba1daac5a63c9563
aayush <ar0671362@gmail.com>
2026-07-14 07:16:44 +0530

    updated description and dimensions

--- src/config/Projects.tsx
+++ src/config/Projects.tsx
@@ -24,8 +24,8 @@ export const projects: Project[] = [
   {
     title: 'Lonestar academy',
     description:
-      'A comprehensive study platform with notes, flashcards, quizzes, AI chatbot, and interactive learning tools',
-    image: '/project/lonestar.png',
+      'Developed a custom institutional website for Lonestar Academy using Next.js, Node.js, Express.js, MongoDB, Cloudinary, and Hostinger VPS. Built a fully dynamic custom CMS with an Admin Dashboard that enables administrators to manage the entire website without developer intervention. Implemented features for creating and managing courses with pricing, duration, descriptions, curriculum, images, and blogs, along with a centralized enquiry management system. Developed a complete student enrollment workflow with secure document uploads (ID proofs, educational certificates, and other supporting documents), integrated online payment gateway for course enrollments, and used Cloudinary for optimized media and document storage. Deployed and configured the backend on Hostinger VPS, delivering a scalable, secure, and easy-to-manage platform.',
+    image: '/project/lone.png',
     video: 'https://ik.imagekit.io/hokb3mrdr/notesbuddy.mp4?tr=orig',
     link: 'https://notesbuddy.in',
     technologies: [
