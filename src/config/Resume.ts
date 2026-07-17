8ce958a20d897bc6b6c7c48f3be277fdd98551b5
aayush <ar0671362@gmail.com>
2026-07-17 22:33:30 +0530

    resume attached

--- src/config/Resume.ts
+++ src/config/Resume.ts
@@ -1,3 +1,4 @@
 export const resumeConfig = {
-  url: 'https://drive.google.com/file/d/1ormIiMVpWGAMOZ3FZVj_XrKPkEmPlPQj/preview',
+  // url: 'https://drive.google.com/file/d/1ormIiMVpWGAMOZ3FZVj_XrKPkEmPlPQj/preview',
+  url:"https://drive.google.com/file/d/1n4GrmcCcKyLCSduMCgfmJwy4empVb9yc/preview"
 };
